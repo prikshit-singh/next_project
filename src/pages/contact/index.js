29867e5b4337d15f3811ac23e6eed0f6208e0111
prikshit-singh <prikshitlatherlather@gmail.com>
2023-04-21 23:51:38 +0530

    11:00pm

---
+++ src/pages/contact/index.js
@@ -0,0 +1,16 @@
+import  Head  from 'next/head';
+
+export default function index() {
+    return (
+
+        <div>
+            <Head>
+                <title>My page title</title>
+            </Head>
+            <p>Hello world!</p>
+        </div>
+
+    );
+}
+
+//  index;
