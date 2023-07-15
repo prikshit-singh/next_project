5dde92cb99e33dd68bc69b8ca25280c3ee6ae994
anju-malik <anjumalik0312@gmail.com>
2023-07-15 15:24:18 +0530

    3:24 pm cc

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,27 @@
-import React from 'react';
-import Link from 'next/link'
-import styles from '../styles/Navbar.module.css'
+import React from "react";
+import Link from "next/link";
+import styles from "../styles/Navbar.module.css";
+import { Search } from "@mui/icons-material";
 function Navbar(props) {
   return (
     <>
-      <div className={styles.navBarMainDiv} >
-        
-          <header className={styles.navBarSeconderyDiv}>
+      <div className={styles.navBarMainDiv}>
+        <header className={styles.navBarSeconderyDiv}>
           <div className={styles.logoDiv}>
-              <h1>LOGO</h1>
+            <h1>LOGO</h1>
+            <input
+              className={styles.Search}
+              type="search"
+              placeholder="search"
+            />
           </div>
-          <div className={styles.navigationDiv}>
-          <Link href='/'>
-              Home
-            </Link>
-            <Link href='/about'>
-              About
-            </Link>
-            <Link href='/write'>
-              Write
-            </Link>
-            </div>
-            
-          </header>
 
-      
+          <div className={styles.navigationDiv}>
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+            <Link href="/write">Write</Link>
+          </div>
+        </header>
       </div>
     </>
   );
