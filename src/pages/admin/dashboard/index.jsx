eec00d759c345dfdff2f58324180661a17e12a21
prikshit-singh <prikshitlatherlather@gmail.com>
2023-10-08 20:23:18 +0530

    cc

--- src/pages/admin/dashboard/index.jsx
+++ src/pages/admin/dashboard/index.jsx
@@ -1,14 +1,15 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import Navadmin from '../../../components/frontEndComponent/navabrs/adminNavbar/Navadmin';
 import { withAuth } from '../../../components/Auth';
 import MultiSelectUniversity from '../../../components/frontEndComponent/adminComponents/lists/university/MultiSelectUniversity';
 import Subnavadmin from '../../../components/frontEndComponent/navabrs/adminNavbar/Subnavadmin';
 import style from '../../../styles/admin/Dashboard.module.css'
+
 const index = () => {
     return (
         <>
             <Navadmin />
-            <Subnavadmin  name="UNIVERSITY" state="state"/>
+            <Subnavadmin name="UNIVERSITY" state="state" />
 
             <div className={style.MultiSelect} >
                 <MultiSelectUniversity />
