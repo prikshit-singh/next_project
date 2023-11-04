19cdcb0444a31b3b1ea8caf7acd98ec8f33904cf
anju-malik <43513459+prikshit-singh@users.noreply.github.com>
2023-11-04 23:08:57 +0530

    cc

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,27 +5,17 @@ const Footer = () => {
   return (
     <>
       <div className={styles.footerMainDiv}>
-        <div className={styles.footerMainDiv2}>
-          <div className={styles.logoDiv2}>
-            <img
-              style={{ height: "40px" }}
-              src={`https://gitgurus.com/favicon.png`}
-              alt="gitgurus.com logo"
-            />
-          </div>
-          <div className={styles.copyright}>
-            @-All Rights Reserved
+           <div className={styles.copyright}>
+           copyright
             <Link className={styles.footerLinks} href="/">
               Terms
             </Link>
             <Link className={styles.footerLinks} href="/privacy">
-
-              Privacy
+             Privacy
             </Link>
+            </div>
           </div>
-        </div>
-      </div>
-    </>
+        </>
   );
 };
 
